e3cfcf6a96a4f1e2fb2014d223651bf87e0ed340
mariamoratalla <maria.moratalla97@gmail.com>
2024-03-03 11:51:56 +0100

    apartado 1

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,114 @@
-import "./style.css";
+const estilo = "color: salmon; font-size: 16px; font-weight: bold";
 
-console.log("Hello Typescript!");
+type Especialidad = "Medico de familia" | "Pediatra" | "Cardiólogo";
+
+interface Pacientes {
+  id: number;
+  nombre: string;
+  apellidos: string;
+  sexo: string;
+  temperatura: number;
+  frecuenciaCardiaca: number;
+  especialidad: Especialidad;
+  edad: number;
+}
+
+const pacientes: Pacientes[] = [
+  {
+    id: 1,
+    nombre: "John",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Medico de familia",
+    edad: 44,
+  },
+  {
+    id: 2,
+    nombre: "Jane",
+    apellidos: "Doe",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 70,
+    especialidad: "Medico de familia",
+    edad: 43,
+  },
+  {
+    id: 3,
+    nombre: "Junior",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 90,
+    especialidad: "Pediatra",
+    edad: 8,
+  },
+  {
+    id: 4,
+    nombre: "Mary",
+    apellidos: "Wien",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 120,
+    especialidad: "Medico de familia",
+    edad: 20,
+  },
+  {
+    id: 5,
+    nombre: "Scarlett",
+    apellidos: "Somez",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 110,
+    especialidad: "Cardiólogo",
+    edad: 30,
+  },
+  {
+    id: 6,
+    nombre: "Brian",
+    apellidos: "Kid",
+    sexo: "Male",
+    temperatura: 39.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Pediatra",
+    edad: 11,
+  },
+];
+
+/* APARTADO 1 */
+console.log("%cAPARTADO 1", estilo);
+
+/* Extraer la lista de pacientes que están asignados a la especialidad de Pediatría */
+
+const obtenPacientesAsignadosAPediatria = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  const arrayPediatria: Pacientes[] = pacientes.filter(
+    (paciente) => paciente.especialidad === "Pediatra"
+  );
+  return arrayPediatria;
+};
+
+console.log(
+  "Los pacientes asignados a pediatría son:",
+  obtenPacientesAsignadosAPediatria(pacientes)
+);
+
+/* Extraer la lista de pacientes asignados a Pediatría y que tengan una edad menor de 10 años */
+
+const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  const arrayPediatriaMenorDiezAños: Pacientes[] = pacientes.filter(
+    (paciente) => paciente.especialidad === "Pediatra" && paciente.edad < 10
+  );
+  return arrayPediatriaMenorDiezAños;
+};
+
+console.log(
+  "Los pacientes asignados a Pediatría y menores de 10 años son: ",
+  obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes)
+);
+
+
